865739aa83691fb0b08c2c5bd4562fdbcfff181c
Joseph <aririjoseph@gmail.com>
2023-05-09 15:50:30 +0100

    added a new background

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
 from .models import Room
 
+
 class roomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['host', 'participants']
